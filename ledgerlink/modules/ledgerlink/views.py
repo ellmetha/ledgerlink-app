bdede07610c6e950a643c546b1721b6a1fdab84f
Morgan Aubert <morgan.aubert@zoho.com>
2018-03-03 23:31:32 -0500

    Updated views

--- ledgerlink/modules/ledgerlink/views.py
+++ ledgerlink/modules/ledgerlink/views.py
@@ -26,12 +26,18 @@ def redirect_code(code):
     """ Redirects the user to the URL associated with considered code if applicable. """
     client = get_client()
 
+    # Tries to get the corresponding URL by invoking the smart contract using the JSON-RPC client.
+    # This will not result in a transaction being committed to the NEO blockchain because we are
+    # using the 'invoke' JSON-RPC method: the getURL function will be run inside the NEO Virtual
+    # Machine but the contract's storage won't be updated at all.
     try:
         result = client.contract(
             current_app.config.get('LEDGERLINK_CONTRACT_SCRIPT_HASH')).getURL(code)
     except ConnectionError:
         return redirect(url_for('ledgerlink.home', neodown='yes'))
 
+    # Processes the response sent back by the NEO node and tries to get the URL corresponding to the
+    # considered code if it was found in the contract's storage.
     try:
         url = result['stack'][0]['value']
         url = url.decode('utf-8')
